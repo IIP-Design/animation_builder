ce6b8e4d1253a7e69f00f84b91b3f30da01a7f87
mrewers <mrewers@users.noreply.github.com>
2019-09-25 12:47:07 -0400

    Add basic config files

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: "preserve",
+  quoteProps: "consistent",
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+};
