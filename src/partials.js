c105787412313c0b4b01427f4d7c603ff6fc1232
mrewers <mrewers@users.noreply.github.com>
2019-10-21 11:58:24 -0400

    Add slide animation

--- src/partials.js
+++ src/partials.js
@@ -1,10 +1,10 @@
 module.exports = {
   // example: 'src/example/example.html',
-  hero: '/src/hero/hero.html',
-  highlight: '/src/highlight/highlight.html',
-  quiz: '/src/quiz/quiz.html',
-  resources: '/src/resources/resources.html',
-  slides: '/src/slides/slides.html',
-  stats: '/src/stats/stats.html',
-  timeline: '/src/timeline/timeline.html'
+  // hero: '/src/hero/hero.html',
+  // highlight: '/src/highlighted/highlighted.html',
+  // quiz: '/src/quiz/quiz.html',
+  // resources: '/src/resources/resources.html',
+  slides: '/src/slides/slides.html'
+  // stats: '/src/stats/stats.html',
+  // timeline: '/src/timeline/timeline.html'
 };
