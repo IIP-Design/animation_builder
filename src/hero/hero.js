8256ba50ea55afba42530e6563d830c5aaae84fa
mrewers <mrewers@users.noreply.github.com>
2019-10-29 17:03:47 -0400

    Use ES6 features

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -1,11 +1,11 @@
-// Import any CSS/SCSS files relevant to your module at the top of the js file
+import { TimelineMax, Power2 } from 'gsap';
+
 import './hero.scss';
-import { TimelineMax, Power2 } from 'gsap'; // GreenSock
 
-const tl = new TimelineMax({ repeat: -1 });
-const line = document.getElementsByClassName('hero-line');
-const lineArray = Array.from(line);
+const tl = new TimelineMax( { repeat: -1 } );
+const line = document.getElementsByClassName( 'hero-line' );
+const lineArray = [...line];
 
-lineArray.forEach(function runLines(element) {
-  tl.to(element, 2, { opacity: 1 }).to(element, 2, { opacity: 0, ease: Power2.easeIn }, '+=1');
-});
+lineArray.forEach( element => {
+  tl.to( element, 2, { opacity: 1 } ).to( element, 2, { opacity: 0, ease: Power2.easeIn }, '+=1' );
+} );
