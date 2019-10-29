c8faba09e83a493ca4853179baf4cfcd1faa07b6
mrewers <mrewers@users.noreply.github.com>
2019-10-29 11:53:29 -0400

    Update styling on test page

--- src/partials.js
+++ src/partials.js
@@ -4,7 +4,7 @@ module.exports = {
   // highlight: '/src/highlighted/highlighted.html',
   // quiz: '/src/quiz/quiz.html',
   // resources: '/src/resources/resources.html',
+  timeline: '/src/timeline/timeline.html',
   slides: '/src/slides/slides.html'
   // stats: '/src/stats/stats.html',
-  // timeline: '/src/timeline/timeline.html'
 };
