09fc0150db61e081b65e001f911ee85d9d0ca880
mrewers <mrewers@users.noreply.github.com>
2019-10-31 10:33:15 -0400

    Nest CSS, include quiz in partials list

--- src/_iran/partials.js
+++ src/_iran/partials.js
@@ -2,8 +2,8 @@ module.exports = {
   hero: '/src/_iran/hero/hero.html',
   stats: '/src/_iran/stats/stats.html',
   timeline: '/src/_iran/timeline/timeline.html',
-  slides: '/src/_iran/slides/slides.html'
+  slides: '/src/_iran/slides/slides.html',
   // highlight: '/src/_iran/highlighted/highlighted.html',
-  // quiz: '/src/_iran/quiz/quiz.html',
+  quiz: '/src/_iran/quiz/quiz.html'
   // resources: '/src/_iran/resources/resources.html',
 };
--- src/_iran/quiz/quiz.js
+++ src/_iran/quiz/quiz.js
@@ -1,4 +1,1 @@
-// Import any CSS/SCSS files relevant to your module at the top of the js file
 import './quiz.scss';
-// import ScrollMagic from 'scrollmagic'; // ScrollMagic
-// import { TweenMax } from "gsap/TweenMax"; // GreenSock
