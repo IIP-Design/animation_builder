7f940ab9535473df2b16a776935299cf5d3ecbab
Steven Hascher <shascher@gmail.com>
2019-10-31 13:38:38 -0600

    Adds Resources section functionality

--- src/_iran/resources/resources.js
+++ src/_iran/resources/resources.js
@@ -1,4 +1,14 @@
 // Import any CSS/SCSS files relevant to your module at the top of the js file
+import { toggleItems } from '../../utils/toggle';
+
 import './resources.scss';
-// import ScrollMagic from 'scrollmagic'; // ScrollMagic
-// import { TweenMax } from "gsap/TweenMax"; // GreenSock
+
+const boxes = document.querySelectorAll('.section');
+const buttons = document.querySelectorAll('.card-button');
+
+const boxArray = [...boxes];
+const btnArray = [...buttons];
+
+btnArray.forEach(btn => {
+  btn.addEventListener('click', () => toggleItems(boxArray, btn.dataset.id));
+});
