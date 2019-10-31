20ed8c51e5fb9941575d0457c3bb4ce17180abe8
mrewers <mrewers@users.noreply.github.com>
2019-10-31 15:50:56 -0400

    Animate the quiz cards

--- src/_iran/quiz/quiz.js
+++ src/_iran/quiz/quiz.js
@@ -1,9 +1,37 @@
+import { TweenLite, Back } from 'gsap';
+
 import { toggleItems } from '../../utils/toggle';
 
 import './quiz.scss';
 
-const boxes = document.querySelectorAll( '.true-false-slider-box' );
-const buttons = document.querySelectorAll( '.true-false-button' );
+// Get cards
+const cards = document.querySelectorAll( '.tf-card' );
+const cardArray = [...cards];
+
+// Preset properties on cards
+TweenLite.set( '.tf-card', { perspective: 400 } );
+TweenLite.set( '.back', { rotationX: 180 } );
+TweenLite.set( ['.back', '.front'], { backfaceVisibility: 'hidden' } );
+
+const rotate = card => {
+  TweenLite.to( card, 2, {
+    ease: Back.easeIn,
+    rotationX: -180,
+    transformStyle: 'preserve-3d'
+  } );
+};
+
+const rotateBack = card => {
+  TweenLite.to( card, 2, { rotationX: 0 } );
+};
+
+// Rotate cards on hover
+cardArray.forEach( card => {
+  card.addEventListener( 'mouseover', () => rotate( card ) );
+  card.addEventListener( 'mouseleave', () => rotateBack( card ) );
+} );
+
+// Navigate between slides...
 
 const boxArray = [...boxes];
 const btnArray = [...buttons];
