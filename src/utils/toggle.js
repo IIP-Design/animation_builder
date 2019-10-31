c0cec1f9880717f1718a36f4d418d4e11a503d08
mrewers <mrewers@users.noreply.github.com>
2019-10-31 15:47:44 -0400

    Add check to ensure that id is a string

--- src/utils/toggle.js
+++ src/utils/toggle.js
@@ -1,8 +1,12 @@
 export const toggleItems = ( arr, id ) => {
+  // Since the id pulled off of the DOM element is always a string,
+  // we need to ensure that the id passed in is a string.
+  const strId = id.toString();
+
   arr.forEach( item => {
-    if ( item.id === id && item.classList.contains( 'hidden' ) ) {
+    if ( item.id === strId && item.classList.contains( 'hidden' ) ) {
       item.classList.remove( 'hidden' );
-    } else if ( item.id !== id && !item.classList.contains( 'hidden' ) ) {
+    } else if ( item.id !== strId && !item.classList.contains( 'hidden' ) ) {
       item.classList.add( 'hidden' );
     }
   } );
