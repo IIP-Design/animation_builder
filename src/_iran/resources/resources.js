e208fd9c4729da0fd5b59600082c86e0793c7f03
mrewers <mrewers@users.noreply.github.com>
2019-10-31 16:16:52 -0400

    Rename some variables from box to section to more accurately represent selected DOM elements

--- src/_iran/resources/resources.js
+++ src/_iran/resources/resources.js
@@ -1,14 +1,13 @@
-// Import any CSS/SCSS files relevant to your module at the top of the js file
 import { toggleItems } from '../../utils/toggle';
 
 import './resources.scss';
 
-const boxes = document.querySelectorAll('.section');
-const buttons = document.querySelectorAll('.card-button');
+const sections = document.querySelectorAll( '.section' );
+const buttons = document.querySelectorAll( '.card-button' );
 
-const boxArray = [...boxes];
+const sectionArray = [...sections];
 const btnArray = [...buttons];
 
-btnArray.forEach(btn => {
-  btn.addEventListener('click', () => toggleItems(boxArray, btn.dataset.id));
-});
+btnArray.forEach( btn => {
+  btn.addEventListener( 'click', () => toggleItems( sectionArray, btn.dataset.id ) );
+} );
