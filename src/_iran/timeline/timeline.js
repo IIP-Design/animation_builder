2565b712841795c4100c4f806e552aee75a64bd6
mrewers <mrewers@users.noreply.github.com>
2019-11-05 15:36:23 -0500

    Change slide title as part of animation

--- src/_iran/timeline/timeline.js
+++ src/_iran/timeline/timeline.js
@@ -8,49 +8,62 @@ import './timeline.scss';
 const timelineSection = document.getElementById( 'timeline-section' );
 const distanceFromTop = timelineSection.getBoundingClientRect().top;
 
-// Initialize timelines
-const tl = new TimelineMax( { paused: true, repeat: 0 } );
-const tl2 = new TimelineMax( { paused: true, repeat: 0 } );
-
 // Get all elements upons which we will be acting
 const cards = document.getElementsByClassName( 'timeline-card' );
 const dots = document.getElementsByClassName( 'timeline-dot' );
+const titles = document.getElementsByClassName( 'timeline-slide-title' );
 
 // Add selected elements to an array
 const cardArray = [...cards];
 const dotArray = [...dots];
+const titleArray = [...titles];
 
 // Set color values
 const activeColor = '#d01319';
 const inactiveColor = '#0a314d';
 
 // Define how each timeline behaves
-const runTimeline = ( arr1, arr2 ) => {
+const runTimeline = ( arr1, arr2, arr3 ) => {
   const activeBg = { backgroundColor: activeColor };
   const inactiveBg = { backgroundColor: inactiveColor };
 
+  // Initialize timelines
+  const tlDots = new TimelineMax( { repeat: -1 } );
+  const tlText = new TimelineMax( { repeat: -1 } );
+  const tlTitle = new TimelineMax( { repeat: -1 } );
+
   arr1.forEach( el => {
-    tl.fromTo( el, 2, inactiveBg, activeBg )
-      .to( el, 2, inactiveBg )
-      .play();
+    tlDots.fromTo( el, 2, inactiveBg, activeBg ).to( el, 2, inactiveBg );
   } );
 
   arr2.forEach( el => {
-    tl2
+    tlText
       .fromTo( el.children, 2, { color: inactiveColor }, { color: activeColor } )
-      .to( el.children, 2, { color: inactiveColor } )
-      .play();
+      .to( el.children, 2, { color: inactiveColor } );
+  } );
+
+  arr3.forEach( el => {
+    tlTitle.fromTo( el, 2, { display: 'none' }, { display: 'block' } ).to( el, 2, { display: 'none' } );
   } );
 };
 
 // Kick off timeline animation when section reaches top of the screen
 const playWhenAtTop = e => {
-  const off = getScrollOffsets();
+  const off = getScrollOffsets().y;
 
-  if ( off.y >= distanceFromTop && cards && dots && cardArray.length > 0 && dotArray.length > 0 ) {
-    runTimeline( dotArray, cardArray );
+  if (
+    cards &&
+    dots &&
+    titles &&
+    cardArray.length > 0 &&
+    dotArray.length > 0 &&
+    titleArray.length > 0 &&
+    timelineSection &&
+    off >= distanceFromTop
+  ) {
+    runTimeline( dotArray, cardArray, titleArray );
+    document.removeEventListener( 'scroll', playWhenAtTop );
   }
 };
 
-document.addEventListener( 'mousewheel', playWhenAtTop );
-document.addEventListener( 'DOMMouseScroll', playWhenAtTop );
+document.addEventListener( 'scroll', playWhenAtTop );
