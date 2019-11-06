4c8218550946ba680e1e126509fb08d87158fada
mrewers <mrewers@users.noreply.github.com>
2019-11-06 11:43:52 -0500

    Add true/false quiz block

--- src/_5g/partials.js
+++ src/_5g/partials.js
@@ -1,4 +1,5 @@
 module.exports = {
   hero: '/src/_5g/hero/hero.html',
-  promise: '/src/_5g/promise/promise.html'
+  promise: '/src/_5g/promise/promise.html',
+  quiz: '/src/_5g/quiz/quiz.html'
 };
