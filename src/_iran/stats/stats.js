0710dd15ce433053a44c1ed08b88bb6d55eaf63a
mrewers <mrewers@users.noreply.github.com>
2019-11-07 17:34:12 -0500

    Simplify stats section with single function and fewer variables

--- src/_iran/stats/stats.js
+++ src/_iran/stats/stats.js
@@ -3,70 +3,66 @@ import ScrollMagic from 'scrollmagic';
 
 import './stats.scss';
 
-const statOne = document.getElementById('stat-1');
-const statOneData = statOne.getAttribute('data-stat');
-const statTwo = document.getElementById('stat-2');
-const statTwoData = statTwo.getAttribute('data-stat');
-const statThree = document.getElementById('stat-3');
-const statThreeData = statThree.getAttribute('data-stat');
-const counter = { var: 0 };
-const counterTwo = { var: 0 };
-const counterThree = { var: 0 };
+const statOne = document.getElementById( 'stat-1' );
+const statTwo = document.getElementById( 'stat-2' );
+const statThree = document.getElementById( 'stat-3' );
 
-// Get the Div for Targeting
-const start = document.querySelector('.hero-content-background');
+// Initialize counters with 0 values
+const counter = {
+  one: { val: 0 },
+  two: { val: 0 },
+  three: { val: 0 }
+};
 
-// Individual Counting Functions
-function countUpOne() {
-  statOne.innerHTML = Math.ceil(counter.var);
-}
-function countUpTwo() {
-  statTwo.innerHTML = Math.ceil(counterTwo.var);
-}
-function countUpThree() {
-  statThree.innerHTML = Math.ceil(counterThree.var);
-}
+// Change the value displayed by the stats block
+const countUp = ( el, count ) => {
+  el.innerHTML = Math.ceil( count.val );
+};
 
-// Count Up All Stats
-function runStats() {
-  const tl = new TimelineMax(); // Set Up New Timeline for Tweens
+// Run the stats animation
+const runStats = () => {
+  const tl = new TimelineMax(); // Set up new timeline for tweens
 
-  tl.to(counter, 5, {
-    var: statOneData,
-    onUpdate: countUpOne,
-    ease: Power2.easeOut
-  })
-    .to(
-      counterTwo,
-      5,
-      {
-        var: statTwoData,
-        onUpdate: countUpTwo,
-        ease: Power2.easeOut
-      },
-      0 // Start without delay
-    )
-    .to(
-      counterThree,
-      5,
-      {
-        var: statThreeData,
-        onUpdate: countUpThree,
-        ease: Power2.easeOut
-      },
-      0 // Start without Delay
-    );
-}
+  if ( statOne && statTwo && statThree ) {
+    tl.to( counter.one, 5, {
+      val: statOne.getAttribute( 'data-stat' ),
+      onUpdate: () => countUp( statOne, counter.one ),
+      ease: Power2.easeOut
+    } )
+      .to(
+        counter.two,
+        5,
+        {
+          val: statTwo.getAttribute( 'data-stat' ),
+          onUpdate: () => countUp( statTwo, counter.two ),
+          ease: Power2.easeOut
+        },
+        0 // Start without delay
+      )
+      .to(
+        counter.three,
+        5,
+        {
+          val: statThree.getAttribute( 'data-stat' ),
+          onUpdate: () => countUp( statThree, counter.three ),
+          ease: Power2.easeOut
+        },
+        0 // Start without Delay
+      );
+  }
+};
+
+// Set the animation trigger using Scroll Magic
+const start = document.querySelector( '.hero-content-background' );
 
 const controller = new ScrollMagic.Controller();
 
-if (start) {
-  new ScrollMagic.Scene({
+if ( start ) {
+  new ScrollMagic.Scene( {
     triggerElement: start,
     triggerHook: 'onLeave',
     duration: '100%'
-  })
-    // .setPin(start)
-    .setTween(runStats)
-    .addTo(controller);
+  } )
+    .setTween( runStats )
+    .addTo( controller );
 }
