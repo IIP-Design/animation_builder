e5895ea7b4b7e1641303ed2becdecbad1f07743c
mrewers <mrewers@users.noreply.github.com>
2019-11-08 16:05:08 -0500

    Load background gradient rather than image for hero block when on mobile

--- src/_5g/hero/hero.js
+++ src/_5g/hero/hero.js
@@ -1,1 +1,12 @@
 import './hero.scss';
+
+const width = Math.max( document.documentElement.clientWidth, window.innerWidth || 0 );
+
+const bg = document.getElementById( 'hero-content-background' );
+
+if ( bg && width < 750 ) {
+  bg.setAttribute(
+    'style',
+    'background-image: none; background: linear-gradient(180deg, rgba(0,15,42,1) 0%, rgba(9,9,121,1) 78%, rgba(4,110,187,1) 100%);'
+  );
+}
