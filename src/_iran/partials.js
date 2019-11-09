354e097d19700c0cd544a942ea76561ef8a04a49
mrewers <mrewers@users.noreply.github.com>
2019-11-08 20:13:28 -0500

    Delete out of use highlighted block

--- src/_iran/partials.js
+++ src/_iran/partials.js
@@ -4,7 +4,6 @@ module.exports = {
   timeline: '/src/_iran/timeline/timeline.html',
   slides: '/src/_iran/slides/slides.html',
   position: '/src/_iran/position/position.html',
-  // highlight: '/src/_iran/highlighted/highlighted.html',
   quiz: '/src/_iran/quiz/quiz.html',
   resources: '/src/_iran/resources/resources.html'
 };
