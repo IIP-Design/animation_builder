4c6fd5ae66b477e24a0fd32709d333a200436df8
mrewers <mrewers@users.noreply.github.com>
2019-11-08 20:13:56 -0500

    Switch to using color variables

--- src/_5g/hero/hero.js
+++ src/_5g/hero/hero.js
@@ -7,6 +7,6 @@ const bg = document.getElementById( 'hero-content-background' );
 if ( bg && width < 750 ) {
   bg.setAttribute(
     'style',
-    'background-image: none; background: linear-gradient(180deg, rgba(0,15,42,1) 0%, rgba(9,9,121,1) 78%, rgba(4,110,187,1) 100%);'
+    'background-image: none; background: linear-gradient(180deg, rgba(0,16,39,1) 0%, rgba(9,9,121,1) 71%, rgba(0,45,116,1) 100%);'
   );
 }
