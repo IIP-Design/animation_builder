d916b81351065273580d6f0b05a512b461014429
Steven Hascher <shascher@gmail.com>
2019-11-12 10:07:42 -0700

    Updates to iran blocks and styling

--- src/_iran/resources/resources.js
+++ src/_iran/resources/resources.js
@@ -1,13 +1,25 @@
 import { toggleItems } from '../../utils/toggle';
+import { buildFeed } from '../../utils/cdpFeed/cdpFeed';
 
 import './resources.scss';
 
-const sections = document.querySelectorAll( '.section' );
-const buttons = document.querySelectorAll( '.card-button' );
+const sections = document.querySelectorAll('.section');
+const buttons = document.querySelectorAll('.card-button');
 
 const sectionArray = [...sections];
 const btnArray = [...buttons];
 
-btnArray.forEach( btn => {
-  btn.addEventListener( 'click', () => toggleItems( sectionArray, btn.dataset.id ) );
-} );
+btnArray.forEach(btn => {
+  btn.addEventListener('click', () => toggleItems(sectionArray, btn.dataset.id));
+});
+
+// Adds feed of CDP content to the block
+const women = document.getElementById('resource-women-cdp-feed');
+const freedom = document.getElementById('resource-freedom-cdp-feed');
+const workers = document.getElementById('resource-workers-cdp-feed');
+
+if (women && freedom && workers) {
+  buildFeed(women);
+  buildFeed(freedom);
+  buildFeed(workers);
+}
--- src/_iran/slides/slides.js
+++ src/_iran/slides/slides.js
@@ -7,31 +7,31 @@ import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugin
 import './slides.scss';
 
 // Get elements that will be acted upon
-const trigger = document.getElementById( 'slide-container' );
-const slideTwo = document.getElementById( 'slide-two' );
-const slideThree = document.getElementById( 'slide-three' );
-const slideFour = document.getElementById( 'slide-four' );
-const slideFive = document.getElementById( 'slide-five' );
-const slideSix = document.getElementById( 'slide-six' );
-const slideSeven = document.getElementById( 'slide-seven' );
+const trigger = document.getElementById('slide-container');
+const slideTwo = document.getElementById('slide-two');
+const slideThree = document.getElementById('slide-three');
+const slideFour = document.getElementById('slide-four');
+const slideFive = document.getElementById('slide-five');
+const slideSix = document.getElementById('slide-six');
+const slideSeven = document.getElementById('slide-seven');
 
 const wipe = new TimelineLite()
-  .fromTo( slideTwo, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .fromTo( slideThree, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .fromTo( slideFour, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .fromTo( slideFive, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .fromTo( slideSix, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .fromTo( slideSeven, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' );
+  .fromTo(slideTwo, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .fromTo(slideThree, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .fromTo(slideFour, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .fromTo(slideFive, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .fromTo(slideSix, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .fromTo(slideSeven, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1');
 
 const controller = new ScrollMagic.Controller();
 
-if ( trigger && slideTwo && slideThree && slideFour && slideFive && slideSix && slideSeven ) {
-  new ScrollMagic.Scene( {
+if (trigger && slideTwo && slideThree && slideFour && slideFive && slideSix && slideSeven) {
+  new ScrollMagic.Scene({
     triggerElement: trigger,
     triggerHook: 'onLeave',
-    duration: '300%'
-  } )
-    .setPin( trigger )
-    .setTween( wipe )
-    .addTo( controller );
+    duration: '2000%'
+  })
+    .setPin(trigger)
+    .setTween(wipe)
+    .addTo(controller);
 }
