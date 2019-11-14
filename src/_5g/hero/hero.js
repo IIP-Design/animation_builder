527849554a3f6ba99ce9fc9a330e9b4021b45030
mrewers <mrewers@users.noreply.github.com>
2019-11-14 16:27:21 -0500

    Keep nav buttons same size

--- src/_5g/hero/hero.js
+++ src/_5g/hero/hero.js
@@ -5,8 +5,5 @@ const width = Math.max( document.documentElement.clientWidth, window.innerWidth 
 const bg = document.getElementById( 'hero-content-background' );
 
 if ( bg && width < 750 ) {
-  bg.setAttribute(
-    'style',
-    'background-image: none; background: linear-gradient(180deg, rgba(0,16,39,1) 0%, rgba(9,9,121,1) 71%, rgba(0,45,116,1) 100%);'
-  );
+  bg.setAttribute( 'style', 'background-image: none;' );
 }
