7f10367384635116190da383fcd7b62188fb146b
Steven Hascher <shascher@gmail.com>
2019-11-18 15:15:12 -0700

    Updates resources section toggle, and toggle global, and quiz section

--- src/_iran/resources/resources.js
+++ src/_iran/resources/resources.js
@@ -3,23 +3,29 @@ import { buildFeed } from '../../utils/cdpFeed/cdpFeed';
 
 import './resources.scss';
 
-const sections = document.querySelectorAll( '.section' );
-const buttons = document.querySelectorAll( '.card-button' );
+const sections = document.querySelectorAll('.section');
+const buttons = document.querySelectorAll('.card-button');
 
 const sectionArray = [...sections];
 const btnArray = [...buttons];
 
-btnArray.forEach( btn => {
-  btn.addEventListener( 'click', () => toggleItems( sectionArray, btn.dataset.id ) );
-} );
+// Change Button Color on Click
+const clickHandler = e => {
+  toggleItems(sectionArray, e.target.id, 'hidden');
+  toggleItems(btnArray, e.target.id, 'inactive');
+};
+
+btnArray.forEach(btn => {
+  btn.addEventListener('click', e => clickHandler(e));
+});
 
 // Adds feed of CDP content to the block
-const women = document.getElementById( 'resource-women-cdp-feed' );
-const freedom = document.getElementById( 'resource-freedom-cdp-feed' );
-const workers = document.getElementById( 'resource-workers-cdp-feed' );
+const women = document.getElementById('resource-women-cdp-feed');
+const freedom = document.getElementById('resource-freedom-cdp-feed');
+const workers = document.getElementById('resource-workers-cdp-feed');
 
-if ( women && freedom && workers ) {
-  buildFeed( women );
-  buildFeed( freedom );
-  buildFeed( workers );
+if (women && freedom && workers) {
+  buildFeed(women);
+  buildFeed(freedom);
+  buildFeed(workers);
 }
