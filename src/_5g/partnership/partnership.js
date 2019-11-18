0dc5a8eef4decc9f4b2edadeb387516c0b1ba4fd
mrewers <mrewers@users.noreply.github.com>
2019-11-18 09:21:41 -0500

    Add addition share stories to partnership section

--- src/_5g/partnership/partnership.js
+++ src/_5g/partnership/partnership.js
@@ -3,7 +3,12 @@ import './partnership.scss';
 import { buildFeed } from '../../utils/cdpFeed/cdpFeed';
 
 const feed = document.getElementById( 'partnership-cdp-feed' );
+const feedTwo = document.getElementById( 'partnership-cdp-feed-two' );
 
 if ( feed ) {
   buildFeed( feed );
+}
+
+if ( feedTwo ) {
+  buildFeed( feedTwo );
 }
