98e3b370822913787f98eb764c5cc9b3dc1f9e81
mrewers <mrewers@users.noreply.github.com>
2019-11-18 17:28:15 -0500

    Add check for e.target, and reset spacing

--- src/_iran/quiz/quiz.js
+++ src/_iran/quiz/quiz.js
@@ -5,101 +5,101 @@ import { toggleItems } from '../../utils/toggle';
 import './quiz.scss';
 
 // Get cards
-const cards = document.querySelectorAll('.tf-card');
+const cards = document.querySelectorAll( '.tf-card' );
 const cardArray = [...cards];
 
 // Preset properties on cards
-TweenLite.set('.tf-card', { perspective: 400 });
-TweenLite.set('.back', { rotationX: 180 });
-TweenLite.set(['.back', '.front'], { backfaceVisibility: 'hidden' });
+TweenLite.set( '.tf-card', { perspective: 400 } );
+TweenLite.set( '.back', { rotationX: 180 } );
+TweenLite.set( ['.back', '.front'], { backfaceVisibility: 'hidden' } );
 
 // Flip the card to show the answer, and then flip back
 const rotate = card => {
-  TweenLite.to(card, 2, {
+  TweenLite.to( card, 2, {
     ease: Back.easeIn,
     rotationX: -180,
     transformStyle: 'preserve-3d'
-  });
+  } );
 
-  TweenLite.to(card, 2, {
+  TweenLite.to( card, 2, {
     delay: 2,
     ease: Back.easeIn,
     rotationX: -360,
     transformStyle: 'preserve-3d'
-  });
+  } );
 };
 
 // Rotate cards on click
-if (cards && cardArray.length > 0) {
-  cardArray.forEach(card => {
-    card.addEventListener('click', () => rotate(card));
-  });
+if ( cards && cardArray.length > 0 ) {
+  cardArray.forEach( card => {
+    card.addEventListener( 'click', () => rotate( card ) );
+  } );
 }
 
 // Navigate between slides...
 
 // Handle counter for mobile
-const counterContainer = document.getElementById('tf-counter');
-const counter = document.createElement('p');
+const counterContainer = document.getElementById( 'tf-counter' );
+const counter = document.createElement( 'p' );
 
-const updateCounter = (el, number, total) => {
+const updateCounter = ( el, number, total ) => {
   el.innerHTML = `${number} / ${total}`;
 };
 
-if (counterContainer && counter) {
-  counterContainer.appendChild(counter);
-  updateCounter(counter, 1, cardArray.length);
+if ( counterContainer && counter ) {
+  counterContainer.appendChild( counter );
+  updateCounter( counter, 1, cardArray.length );
 }
 
 // ...Using arrows
 let currentItem = 1;
 
-const nextArrow = document.querySelector('.tf-carousel-arrow.next');
-const prevArrow = document.querySelector('.tf-carousel-arrow.prev');
+const nextArrow = document.querySelector( '.tf-carousel-arrow.next' );
+const prevArrow = document.querySelector( '.tf-carousel-arrow.prev' );
 
 // Go to next/previous item, or wrap around if at the beginning/end of list
 const goTo = where => {
   const isFirst = currentItem === 1;
   const isLast = currentItem === cardArray.length;
 
-  if (where === 'next' && !isLast) {
+  if ( where === 'next' && !isLast ) {
     currentItem++; // eslint-disable-line no-plusplus
-    toggleItems(cardArray, currentItem, 'hidden');
-    updateCounter(counter, currentItem, cardArray.length);
-  } else if (where === 'next' && isLast) {
+    toggleItems( cardArray, currentItem, 'hidden' );
+    updateCounter( counter, currentItem, cardArray.length );
+  } else if ( where === 'next' && isLast ) {
     currentItem = 1;
-    toggleItems(cardArray, currentItem, 'hidden');
-    updateCounter(counter, currentItem, cardArray.length);
-  } else if (where === 'prev' && !isFirst) {
+    toggleItems( cardArray, currentItem, 'hidden' );
+    updateCounter( counter, currentItem, cardArray.length );
+  } else if ( where === 'prev' && !isFirst ) {
     currentItem--; // eslint-disable-line no-plusplus
-    toggleItems(cardArray, currentItem, 'hidden');
-    updateCounter(counter, currentItem, cardArray.length);
-  } else if (where === 'prev' && isFirst) {
+    toggleItems( cardArray, currentItem, 'hidden' );
+    updateCounter( counter, currentItem, cardArray.length );
+  } else if ( where === 'prev' && isFirst ) {
     currentItem = cardArray.length;
-    toggleItems(cardArray, currentItem, 'hidden');
-    updateCounter(counter, currentItem, cardArray.length);
+    toggleItems( cardArray, currentItem, 'hidden' );
+    updateCounter( counter, currentItem, cardArray.length );
   }
 };
 
-if (nextArrow) {
-  nextArrow.addEventListener('click', () => goTo('next'));
+if ( nextArrow ) {
+  nextArrow.addEventListener( 'click', () => goTo( 'next' ) );
 }
 
-if (prevArrow) {
-  prevArrow.addEventListener('click', () => goTo('prev'));
+if ( prevArrow ) {
+  prevArrow.addEventListener( 'click', () => goTo( 'prev' ) );
 }
 
 // ...Using buttons
-const buttons = document.querySelectorAll('.tf-button');
+const buttons = document.querySelectorAll( '.tf-button' );
 const btnArray = [...buttons];
 
-if (buttons && btnArray.length > 0) {
-  btnArray.forEach(btn => {
+if ( buttons && btnArray.length > 0 ) {
+  btnArray.forEach( btn => {
     const navigateToBox = () => {
-      toggleItems(cardArray, btn.dataset.id, 'hidden');
-      currentItem = Number(btn.dataset.id);
+      toggleItems( cardArray, btn.dataset.id, 'hidden' );
+      currentItem = Number( btn.dataset.id );
     };
 
-    btn.addEventListener('click', navigateToBox);
-  });
+    btn.addEventListener( 'click', navigateToBox );
+  } );
 }
--- src/_iran/resources/resources.js
+++ src/_iran/resources/resources.js
@@ -3,29 +3,31 @@ import { buildFeed } from '../../utils/cdpFeed/cdpFeed';
 
 import './resources.scss';
 
-const sections = document.querySelectorAll('.section');
-const buttons = document.querySelectorAll('.card-button');
+const sections = document.querySelectorAll( '.section' );
+const buttons = document.querySelectorAll( '.card-button' );
 
 const sectionArray = [...sections];
 const btnArray = [...buttons];
 
 // Change Button Color on Click
 const clickHandler = e => {
-  toggleItems(sectionArray, e.target.id, 'hidden');
-  toggleItems(btnArray, e.target.id, 'inactive');
+  if ( e.target && e.target.id ) {
+    toggleItems( sectionArray, e.target.id, 'hidden' );
+    toggleItems( btnArray, e.target.id, 'inactive' );
+  }
 };
 
-btnArray.forEach(btn => {
-  btn.addEventListener('click', e => clickHandler(e));
-});
+btnArray.forEach( btn => {
+  btn.addEventListener( 'click', e => clickHandler( e ) );
+} );
 
 // Adds feed of CDP content to the block
-const women = document.getElementById('resource-women-cdp-feed');
-const freedom = document.getElementById('resource-freedom-cdp-feed');
-const workers = document.getElementById('resource-workers-cdp-feed');
+const women = document.getElementById( 'resource-women-cdp-feed' );
+const freedom = document.getElementById( 'resource-freedom-cdp-feed' );
+const workers = document.getElementById( 'resource-workers-cdp-feed' );
 
-if (women && freedom && workers) {
-  buildFeed(women);
-  buildFeed(freedom);
-  buildFeed(workers);
+if ( women && freedom && workers ) {
+  buildFeed( women );
+  buildFeed( freedom );
+  buildFeed( workers );
 }
