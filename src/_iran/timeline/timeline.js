70303d6b2de2816f094da34b1d2894cfea206bbb
mrewers <mrewers@users.noreply.github.com>
2019-11-19 10:46:25 -0500

    Switch timeline to hover-based transitions on desktop

--- src/_iran/timeline/timeline.js
+++ src/_iran/timeline/timeline.js
@@ -4,19 +4,17 @@ import { getScrollOffsets } from '../../utils/scrolling';
 
 import './timeline.scss';
 
+// Check if mobile
+const isMobile = window.innerWidth <= 768;
+
 // Get the top of the timeline section
 const timelineSection = document.getElementById( 'timeline-section' );
 const distanceFromTop = timelineSection.getBoundingClientRect().top;
 
 // Get all elements upons which we will be acting
-const cards = document.getElementsByClassName( 'timeline-card' );
-const dots = document.getElementsByClassName( 'timeline-dot' );
-const titles = document.getElementsByClassName( 'timeline-slide-title' );
-
-// Add selected elements to an array
-const cardArray = [...cards];
-const dotArray = [...dots];
-const titleArray = [...titles];
+const cards = [...document.getElementsByClassName( 'timeline-card' )];
+const dots = [...document.getElementsByClassName( 'timeline-dot' )];
+const titles = [...document.getElementsByClassName( 'timeline-slide-title' )];
 
 // Set color values
 const activeColor = '#d01319';
@@ -55,17 +53,67 @@ const playWhenAtTop = e => {
     cards &&
     dots &&
     titles &&
-    cardArray.length > 0 &&
-    dotArray.length > 0 &&
-    titleArray.length > 0 &&
+    cards.length > 0 &&
+    dots.length > 0 &&
+    titles.length > 0 &&
     timelineSection &&
     off >= distanceFromTop
   ) {
-    runTimeline( dotArray, cardArray, titleArray );
+    runTimeline( dots, cards, titles );
     document.removeEventListener( 'scroll', playWhenAtTop );
     document.removeEventListener( 'touchmove', playWhenAtTop );
   }
 };
 
-document.addEventListener( 'scroll', playWhenAtTop );
-document.addEventListener( 'touchmove', playWhenAtTop );
+// Add event listeners for automated animation if user is on mobile
+if ( isMobile ) {
+  document.addEventListener( 'scroll', playWhenAtTop );
+  document.addEventListener( 'touchmove', playWhenAtTop );
+}
+
+// Change elements on hover - for use on desktop
+const hoverEffect = el => {
+  if ( el?.dataset?.year ) {
+    const { year } = el.dataset;
+
+    dots.forEach( dot => {
+      if ( dot?.dataset?.year ) {
+        if ( dot.dataset.year === year ) {
+          dot.setAttribute( 'style', `background-color: ${activeColor}` );
+        } else {
+          dot.setAttribute( 'style', `background-color: ${inactiveColor}` );
+        }
+      }
+    } );
+
+    cards.forEach( card => {
+      if ( card?.dataset?.year ) {
+        if ( card.dataset.year === year ) {
+          if ( card.children ) {
+            const children = [...card.children];
+            children.forEach( child => child.setAttribute( 'style', `color: ${activeColor}` ) );
+          }
+        } else if ( card.children ) {
+          const children = [...card.children];
+          children.forEach( child => child.setAttribute( 'style', `color: ${inactiveColor}` ) );
+        }
+      }
+    } );
+
+    titles.forEach( title => {
+      if ( title?.dataset?.year ) {
+        if ( title.dataset.year === year ) {
+          title.setAttribute( 'style', 'display: block' );
+        } else {
+          title.setAttribute( 'style', 'display: none' );
+        }
+      }
+    } );
+  }
+};
+
+// Add event listeners for hover-based changes if user is on desktop
+if ( !isMobile && cards ) {
+  cards.forEach( card => card.addEventListener( 'mouseenter', () => hoverEffect( card ) ) );
+  dots.forEach( dot => dot.addEventListener( 'mouseenter', () => hoverEffect( dot ) ) );
+}
