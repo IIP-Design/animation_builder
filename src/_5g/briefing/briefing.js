a56139fdfae9992a7fdd34edbd8d965eef7a83a0
mrewers <mrewers@users.noreply.github.com>
2019-11-20 17:51:52 -0500

    Remove unused JS

--- src/_5g/briefing/briefing.js
+++ src/_5g/briefing/briefing.js
@@ -1,33 +1,18 @@
 import { toggleItems } from '../../utils/toggle';
-import { buildFeed } from '../../utils/cdpFeed/cdpFeed';
 
 import './briefing.scss';
 
-const sections = document.querySelectorAll('.section');
-const buttons = document.querySelectorAll('.card-button');
-
-const sectionArray = [...sections];
-const btnArray = [...buttons];
+const sections = [...document.querySelectorAll( '.briefing-section' )];
+const buttons = [...document.querySelectorAll( '.briefing-button' )];
 
 // Change Button Color on Click
 const clickHandler = e => {
-  if (e.target && e.target.id) {
-    toggleItems(sectionArray, e.target.id, 'hidden');
-    toggleItems(btnArray, e.target.id, 'inactive');
+  if ( e.target && e.target.id ) {
+    toggleItems( sections, e.target.id, 'hidden' );
+    toggleItems( buttons, e.target.id, 'inactive' );
   }
 };
 
-btnArray.forEach(btn => {
-  btn.addEventListener('click', e => clickHandler(e));
-});
-
-// Adds feed of CDP content to the block
-const women = document.getElementById('resource-women-cdp-feed');
-const freedom = document.getElementById('resource-freedom-cdp-feed');
-const workers = document.getElementById('resource-workers-cdp-feed');
-
-if (women && freedom && workers) {
-  buildFeed(women);
-  buildFeed(freedom);
-  buildFeed(workers);
-}
+buttons.forEach( btn => {
+  btn.addEventListener( 'click', e => clickHandler( e ) );
+} );
