7d2dd4cbb1c91b2e6177e636103871e0bf01bda0
mrewers <mrewers@users.noreply.github.com>
2019-11-26 10:09:17 -0500

    Use JS to remove hidden class on mobile rather than overriding hidden class properties with important

--- src/_5g/briefing/briefing.js
+++ src/_5g/briefing/briefing.js
@@ -2,8 +2,20 @@ import { toggleItems } from '../../utils/toggle';
 
 import './briefing.scss';
 
+// Check if mobile
+const isMobile = window.innerWidth <= 500;
+
 const sections = [...document.querySelectorAll( '.briefing-section' )];
 const buttons = [...document.querySelectorAll( '.briefing-button' )];
+
+// Display all sections if on mobile device
+if ( isMobile && sections ) {
+  sections.forEach( section => {
+    if ( section.classList.contains( 'hidden' ) ) {
+      section.classList.remove( 'hidden' );
+    }
+  } );
+}
 
 // Change Button Color on Click
 const clickHandler = e => {
