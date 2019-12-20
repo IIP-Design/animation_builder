76c986be38dafacd402dc60b255a6df27f87fa09
Steven Hascher <shascher@gmail.com>
2019-12-20 11:39:34 -0500

    updater slider dot updateProgress to get closer to proper position

--- src/_iran/slides/slides.js
+++ src/_iran/slides/slides.js
@@ -7,27 +7,27 @@ import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugin
 import './slides.scss';
 
 // Get elements that will be acted upon
-const trigger = document.getElementById( 'slide-container' );
-const slideTwo = document.getElementById( 'slide-two' );
-const slideThree = document.getElementById( 'slide-three' );
-const slideFour = document.getElementById( 'slide-four' );
-const slideFive = document.getElementById( 'slide-five' );
-const slideSix = document.getElementById( 'slide-six' );
-const slideSeven = document.getElementById( 'slide-seven' );
-const slideEight = document.getElementById( 'slide-eight' );
-const slideNine = document.getElementById( 'slide-nine' );
+const trigger = document.getElementById('slide-container');
+const slideTwo = document.getElementById('slide-two');
+const slideThree = document.getElementById('slide-three');
+const slideFour = document.getElementById('slide-four');
+const slideFive = document.getElementById('slide-five');
+const slideSix = document.getElementById('slide-six');
+const slideSeven = document.getElementById('slide-seven');
+const slideEight = document.getElementById('slide-eight');
+const slideNine = document.getElementById('slide-nine');
 
-const dots = [...document.getElementsByClassName( 'slide-dot' )];
+const dots = [...document.getElementsByClassName('slide-dot')];
 
-const dotOne = document.getElementById( 'slide-dot-one' );
-const dotTwo = document.getElementById( 'slide-dot-two' );
-const dotThree = document.getElementById( 'slide-dot-three' );
-const dotFour = document.getElementById( 'slide-dot-four' );
-const dotFive = document.getElementById( 'slide-dot-five' );
-const dotSix = document.getElementById( 'slide-dot-six' );
-const dotSeven = document.getElementById( 'slide-dot-seven' );
-const dotEight = document.getElementById( 'slide-dot-eight' );
-const dotNine = document.getElementById( 'slide-dot-nine' );
+const dotOne = document.getElementById('slide-dot-one');
+const dotTwo = document.getElementById('slide-dot-two');
+const dotThree = document.getElementById('slide-dot-three');
+const dotFour = document.getElementById('slide-dot-four');
+const dotFive = document.getElementById('slide-dot-five');
+const dotSix = document.getElementById('slide-dot-six');
+const dotSeven = document.getElementById('slide-dot-seven');
+const dotEight = document.getElementById('slide-dot-eight');
+const dotNine = document.getElementById('slide-dot-nine');
 
 const controller = new ScrollMagic.Controller();
 
@@ -38,48 +38,48 @@ const setPosition = pos => {
 };
 
 const wipe = new TimelineLite()
-  .set( dotOne, { background: '#fff' }, '0' )
-  .call( setPosition, [1] )
+  .set(dotOne, { background: '#fff' }, '0')
+  .call(setPosition, [1])
 
-  .fromTo( slideTwo, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotOne, { background: 'none' }, '-=0.5' )
-  .set( dotTwo, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [2] )
+  .fromTo(slideTwo, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotOne, { background: 'none' }, '-=0.5')
+  .set(dotTwo, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [2])
 
-  .fromTo( slideThree, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotTwo, { background: 'none' }, '-=0.5' )
-  .set( dotThree, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [3] )
+  .fromTo(slideThree, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotTwo, { background: 'none' }, '-=0.5')
+  .set(dotThree, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [3])
 
-  .fromTo( slideFour, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotThree, { background: 'none' }, '-=0.5' )
-  .set( dotFour, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [4] )
+  .fromTo(slideFour, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotThree, { background: 'none' }, '-=0.5')
+  .set(dotFour, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [4])
 
-  .fromTo( slideFive, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotFour, { background: 'none' }, '-=0.5' )
-  .set( dotFive, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [5] )
+  .fromTo(slideFive, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotFour, { background: 'none' }, '-=0.5')
+  .set(dotFive, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [5])
 
-  .fromTo( slideSix, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotFive, { background: 'none' }, '-=0.5' )
-  .set( dotSix, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [6] )
+  .fromTo(slideSix, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotFive, { background: 'none' }, '-=0.5')
+  .set(dotSix, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [6])
 
-  .fromTo( slideSeven, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotSix, { background: 'none' }, '-=0.5' )
-  .set( dotSeven, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [7] )
+  .fromTo(slideSeven, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotSix, { background: 'none' }, '-=0.5')
+  .set(dotSeven, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [7])
 
-  .fromTo( slideEight, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotSeven, { background: 'none' }, '-=0.5' )
-  .set( dotEight, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [8] )
+  .fromTo(slideEight, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotSeven, { background: 'none' }, '-=0.5')
+  .set(dotEight, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [8])
 
-  .fromTo( slideNine, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1' )
-  .set( dotEight, { background: 'none' }, '-=0.5' )
-  .set( dotNine, { background: '#fff' }, '-=0.5' )
-  .call( setPosition, [9] );
+  .fromTo(slideNine, 2, { xPercent: 100 }, { xPercent: 0, ease: Linear.easeNone }, '+=1')
+  .set(dotEight, { background: 'none' }, '-=0.5')
+  .set(dotNine, { background: '#fff' }, '-=0.5')
+  .call(setPosition, [9]);
 
 if (
   trigger &&
@@ -92,33 +92,33 @@ if (
   slideEight &&
   slideNine
 ) {
-  new ScrollMagic.Scene( {
+  new ScrollMagic.Scene({
     triggerElement: trigger,
     triggerHook: 'onLeave',
     duration: '1000%'
-  } )
-    .setPin( trigger )
-    .setTween( wipe )
-    .addTo( controller );
+  })
+    .setPin(trigger)
+    .setTween(wipe)
+    .addTo(controller);
 }
 
 const updateProgress = e => {
-  const clickedOn = e?.target?.dataset?.number ? Number( e.target.dataset.number ) : '';
+  const clickedOn = e?.target?.dataset?.number ? Number(e.target.dataset.number) : '';
   const currentPosition = controller.info().scrollPos;
   const fullSlideScroll = controller.info().size;
   const newOffset = clickedOn - position;
   const pixels = newOffset * fullSlideScroll;
-  const scrollAmount = currentPosition + 1.7 * pixels;
+  const scrollAmount = currentPosition + 1.3 * pixels;
 
-  if ( clickedOn !== position ) {
-    controller.scrollTo( scrollAmount );
+  if (clickedOn !== position) {
+    controller.scrollTo(scrollAmount);
   }
 
-  setPosition( clickedOn );
+  setPosition(clickedOn);
 };
 
-if ( dots ) {
-  dots.forEach( dot => {
-    dot.addEventListener( 'click', e => updateProgress( e ) );
-  } );
+if (dots) {
+  dots.forEach(dot => {
+    dot.addEventListener('click', e => updateProgress(e));
+  });
 }
