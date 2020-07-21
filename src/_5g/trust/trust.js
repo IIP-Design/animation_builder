42197ff4c88e27f0b775968ed6ab21b308958f95
mrewers <mrewers@users.noreply.github.com>
2020-07-21 15:53:26 -0400

    Add additional items to article feeds in Trust and 5G Security sections

--- src/_5g/trust/trust.js
+++ src/_5g/trust/trust.js
@@ -2,8 +2,13 @@ import './trust.scss';
 
 import { buildFeed } from '../../utils/cdpFeed/cdpFeed';
 
-const feed = document.getElementById( 'trust-cdp-feed' );
+const feed = document.getElementById('trust-cdp-feed');
+const feedTwo = document.getElementById('trust-cdp-feed-two');
 
-if ( feed ) {
-  buildFeed( feed );
+if (feed) {
+  buildFeed(feed);
+}
+
+if (feedTwo) {
+  buildFeed(feedTwo);
 }
